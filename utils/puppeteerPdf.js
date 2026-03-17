dd3213f47c1464b2ed3b99c73d7562425d788467
rocio <rg3113@yahoo.com>
2026-03-17 11:03:12 -0500

    UPDATED: puppeteerPdf

--- utils/puppeteerPdf.js
+++ utils/puppeteerPdf.js
@@ -1,32 +1,68 @@
-//utils/puppeteerPdf.js
-import puppeteer from "puppeteer";
+// //utils/puppeteerPdf.js
+// import puppeteer from "puppeteer";
+// import chromium from "@sparticuz/chromium-min";
+
+// export async function generatePdfFromHtml(html) {
+//   const browser = await puppeteer.launch({
+//     headless: "new",
+//     args: [
+//       "--no-sandbox",
+//       "--disable-setuid-sandbox",
+//     ],
+//   });
+
+//   const page = await browser.newPage();
+
+//   await page.setContent(html, {
+//     waitUntil: "networkidle0",
+//   });
+
+//   const pdfBuffer = await page.pdf({
+//     format: "Letter",
+//     printBackground: true,
+//     margin: {
+//       top: "1in",
+//       bottom: "1in",
+//       left: "0.75in",
+//       right: "0.75in",
+//     },
+//   });
+
+//   await browser.close();
+//   return pdfBuffer;
+// }
+// utils/puppeteerPdf.js
+import puppeteer from "puppeteer-core";
+import chromium from "@sparticuz/chromium-min";
 
 export async function generatePdfFromHtml(html) {
   const browser = await puppeteer.launch({
-    headless: "new",
-    args: [
-      "--no-sandbox",
-      "--disable-setuid-sandbox",
-    ],
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath(),
+    headless: chromium.headless,
   });
 
-  const page = await browser.newPage();
+  try {
+    const page = await browser.newPage();
 
-  await page.setContent(html, {
-    waitUntil: "networkidle0",
-  });
+    await page.setContent(html, {
+      waitUntil: "networkidle0",
+    });
 
-  const pdfBuffer = await page.pdf({
-    format: "Letter",
-    printBackground: true,
-    margin: {
-      top: "1in",
-      bottom: "1in",
-      left: "0.75in",
-      right: "0.75in",
-    },
-  });
+    const pdfBuffer = await page.pdf({
+      format: "Letter",
+      printBackground: true,
+      margin: {
+        top: "1in",
+        bottom: "1in",
+        left: "0.75in",
+        right: "0.75in",
+      },
+    });
 
-  await browser.close();
-  return pdfBuffer;
+    return Buffer.from(pdfBuffer);
+  } finally {
+    await browser.close();
+  }
 }
